bd4252dfe148c7cd7e56aaec8f5687cf77f16739
Ayanesh Sarkar <ayaneshsarkar101@gmail.com>
2020-10-07 16:12:40 +0530

    Connected to Database :heavy_check_mark:

--- natours/server.js
+++ natours/server.js
@@ -1,5 +1,14 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DB_CONFIG.replace('<PASSWORD>', process.env.DB_PASS);
+
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+}).then(() => console.log('Connected ✔'));
 
 const app = require('./app');
 
