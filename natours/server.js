59f7f73ff43d9241ad63527ce5fb75f8cd9c881d
Ayanesh Sarkar <ayaneshsarkar101@gmail.com>
2020-10-07 23:26:09 +0530

    Created Model tours :heavy_check_mark:

--- natours/server.js
+++ natours/server.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 
+const app = require('./app');
+
 const DB = process.env.DB_CONFIG.replace('<PASSWORD>', process.env.DB_PASS);
 
 mongoose.connect(DB, {
@@ -10,8 +12,32 @@ mongoose.connect(DB, {
   useFindAndModify: false
 }).then(() => console.log('Connected ✔'));
 
-const app = require('./app');
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true
+  },
+  rating: {
+    type: Number,
+    default: 4.8
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price']
+  }
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Forest Camper',
+  rating: 4.3,
+  price: 250
+});
+
+testTour.save().then(doc => console.log(doc)).catch(err => console.log(err));
 
 // Start Server
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on ${port}.`));
