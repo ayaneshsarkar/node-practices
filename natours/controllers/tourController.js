3413b3a0ba304e26cea4caeb08b62792a885148d
Ayanesh Sarkar <ayaneshsarkar101@gmail.com>
2020-11-22 20:59:32 +0530

    Initial Commit :heavy_check_mark:

--- natours/controllers/tourController.js
+++ natours/controllers/tourController.js
@@ -34,23 +34,23 @@ exports.createTour = async (req, res) => {
 }
 
 //Update Tour
-exports.updateTour = (req, res) => {
-  // const id = parseInt(req.params.id);
+exports.updateTour = async (req, res) => {
+  try {
+    const updatedTour = await Tour.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+    res.status(200).json({ status: 'success', data: updatedTour });
 
-  // const tour = tours.find(el => el.id === id);
-  res.status(200).json({ 
-    status: 'success'
-    // data: { tour: 'Updated Tour Here...' } 
-  });
+  } catch(err) {
+    res.status(401).json({ status: 'fail', message: err });
+  }
 }
 
 // Delete Tour
-exports.deleteTour = (req, res) => {
-  // const id = parseInt(req.params.id);
-
-  // const tour = tours.find(el => el.id === id);
-  res.status(204).json({ 
-    status: 'success', 
-    data: null 
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(204).json({ status: 'success', data: null });
+  
+  } catch(err) {
+    res.status(401).json({ status: 'fail', message: err });
+  }
 }
